c93c62e9682099abc2e2bb90be2d29712f9a346c
yicheng-Pan <2434601498@qq.com>
2020-10-04 20:38:10 +0800

    Fraction & FractionTest Ampliation #9

--- src/test/java/es/upm/miw/iwvg_devops/rest/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/FractionTest.java
@@ -4,10 +4,17 @@ import es.upm.miw.iwvg_devops.Fraction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FractionTest {
     private Fraction fraction;
+    private int numerator;
+    private int denominator;
+
     @BeforeEach
     void before() {
         fraction = new Fraction(0, 1);
@@ -40,6 +47,46 @@ public class FractionTest {
     void testFractiondecimal(){
         assertEquals(0, fraction.decimal());
     }
-
-
+    @Test
+    void testFractionIsProper(){
+        fraction.getNumerator();
+        fraction.getDenominator();
+        assertEquals(false, fraction.isProper(fraction.getNumerator(),fraction.getDenominator()));
+    }
+    @Test
+    void testFractionisImProper(){
+        fraction.getNumerator();
+        fraction.getDenominator();
+        assertEquals(true, fraction.isImproper(fraction.getNumerator(),fraction.getDenominator()));
+    }
+    @Test
+    void testFractionIsEquivalent(){
+        fraction.getNumerator();
+        fraction.getDenominator();
+        assertEquals(false, fraction.isEquivalent(fraction.getNumerator(),fraction.getDenominator()));
+    }
+    @Test
+    void testAdd(){
+        Fraction fractionTemp;
+        Fraction fractionSum ;
+        fractionTemp=new Fraction(1,1);
+        fractionSum=new Fraction(1,1);
+        assertEquals(fractionSum.toString(),fraction.add(fractionTemp).toString());
+    }
+    @Test
+    void testFractionMultiply() {
+        Fraction fractionTemp;
+        Fraction fractionSum ;
+        fractionTemp=new Fraction(1,1);
+        fractionSum=new Fraction(0,0);
+        assertEquals(fractionSum.toString(),fraction.multiply(fractionTemp).toString());
+    }
+    @Test
+    void testFractionDivide() {
+        Fraction fractionTemp;
+        Fraction fractionSum ;
+        fractionTemp=new Fraction(1,4);
+        fractionSum=new Fraction(0,0);
+        assertEquals(fractionSum.toString(),fraction.divide(fractionTemp).toString());
+    }
 }
